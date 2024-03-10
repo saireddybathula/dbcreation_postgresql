01ceaf281a818aff8bbd9f8e7d1d5ce722075bfa
saireddybathula <96130866+saireddybathula@users.noreply.github.com>
2024-03-11 00:24:20 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,5 +2,5 @@ provider "google" {
   // Provide your project ID and region here
   project = "tool-evaluation-407900"
   region  = "asia-south1"
-  credentials = "${file("credentials.json")}"
+  credentials = "${file("credentialsdb.json")}"
 }
